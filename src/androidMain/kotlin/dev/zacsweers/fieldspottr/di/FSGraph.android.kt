9908e8ba1bc4193c6747b976e78deddd6e012922
Jack Malpasuto <jack@malpasuto.com>
2025-04-26 12:41:43 -0400

    Subgraph breaks parent

--- src/androidMain/kotlin/dev/zacsweers/fieldspottr/di/FSGraph.android.kt
+++ src/androidMain/kotlin/dev/zacsweers/fieldspottr/di/FSGraph.android.kt
@@ -5,14 +5,24 @@ package dev.zacsweers.fieldspottr.di
 import android.content.Context
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.DependencyGraph
+import dev.zacsweers.metro.Extends
 import dev.zacsweers.metro.Provides
 import dev.zacsweers.metro.SingleIn
 
-@DependencyGraph(AppScope::class)
+@DependencyGraph(AppScope::class, isExtendable = true)
 @SingleIn(AppScope::class)
 interface AndroidFSGraph : FSGraph {
   @DependencyGraph.Factory
   fun interface Factory {
     operator fun invoke(@Provides appContext: Context): AndroidFSGraph
+  }
+}
+
+@DependencyGraph
+interface SubGraph {
+
+  @DependencyGraph.Factory
+  interface Factory {
+    fun create(@Extends appGraph: AndroidFSGraph): SubGraph
   }
 }
